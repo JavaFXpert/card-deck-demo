5ae6c502deaafff53d84fd20b18fb4c08da1de21
Oleh Dokuka <shadowgun@i.ua>
2018-04-08 22:59:38 +0300

    some ideas and improvements

--- src/main/java/com/javafxpert/carddeckdemo/util/ShuffleUtils.java
+++ src/main/java/com/javafxpert/carddeckdemo/util/ShuffleUtils.java
@@ -5,7 +5,9 @@ import reactor.core.publisher.Flux;
 import reactor.util.function.Tuple2;
 import reactor.util.function.Tuples;
 
+import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.List;
 
 public class ShuffleUtils {
   private static final Comparator<Card> comparator = (c1, c2) -> c1.getWorth() - c2.getWorth();
@@ -21,46 +23,36 @@ public class ShuffleUtils {
       );
   }
 
-  /**
-   * Will not work :(
-   * @param cardFlux
-   * @return
-   */
   public static Flux<Card> overhandShuffle(Flux<Card> cardFlux) {
-    return cardFlux.collectList()
-      .map(list -> Tuples.of(Flux.fromIterable(list), (int)(Math.random() * (list.size() - 1) + 1)))
-      .flatMapMany(tuple2 ->
-        tuple2.getT1()
-          .skip(tuple2.getT2())
-          .concatWith(tuple2.getT1()
-            .take(tuple2.getT2()))
-      );
 
-    /*
-    int totalCards = cardFlux.count().block().intValue();
-    int maxChunk = 5;
-    int numCardsLeft = totalCards;
-    Flux<Card> overhandShuffledCardFlux = Flux.empty();
+	  return cardFlux.collectList()
+	                 .flatMapIterable(l -> {
+		                 int maxChunk = 5;
+		                 int numCardsLeft = l.size();
+		                 List<Card> overhandShuffledCardFlux = new ArrayList<>();
 
-    while (numCardsLeft > 0) {
-      Flux<Card> tempCardFlux = cardFlux.take(numCardsLeft);
-      int numCardsToTransfer = Math.min((int)(Math.random() * maxChunk + 1), numCardsLeft);
-      overhandShuffledCardFlux = Flux.concat(overhandShuffledCardFlux, tempCardFlux.takeLast(numCardsToTransfer));
+		                 while (numCardsLeft > 0) {
+			                 List<Card> tempCardFlux = l.subList(0, numCardsLeft);
+			                 int numCardsToTransfer = Math.min((int) (Math.random() * maxChunk + 1),
+					                 numCardsLeft);
+			                 overhandShuffledCardFlux.addAll(
+				                 tempCardFlux.subList(tempCardFlux.size() - numCardsToTransfer, tempCardFlux.size())
+			                 );
 
-      numCardsLeft -= numCardsToTransfer;
-    }
-    return overhandShuffledCardFlux;
-    */
-
+			                 numCardsLeft -= numCardsToTransfer;
+		                 }
+		                 return overhandShuffledCardFlux;
+	                 });
   }
 
   public static Flux<Card> riffleShuffle(Flux<Card> cardFlux) {
-    int totalCards = cardFlux.count().block().intValue();
-    Flux<Card> cutCardsA = cardFlux.take(totalCards / 2);
-    Flux<Card> cutCardsB = cardFlux.takeLast(totalCards / 2);
-    Flux<Tuple2<Card, Card>> tuples = Flux.zip(cutCardsA, cutCardsB);
-
-    return tuples.flatMap(tuple2 -> Flux.just(tuple2.getT1(), tuple2.getT2()));
+    return cardFlux
+            .collectList()
+            .flatMapMany(l -> Flux.zip(
+                Flux.fromStream(l.stream().skip(l.size() / 2)),
+	            Flux.fromStream(l.stream().skip(l.size() / 2))
+            ))
+            .flatMap(tuple2 -> Flux.just(tuple2.getT1(), tuple2.getT2()));
   }
 
   public static Flux<Card> dealPokerHand(Flux<Card> cardFlux) {
