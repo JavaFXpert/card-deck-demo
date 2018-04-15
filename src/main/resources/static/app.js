889a919d75cc85a1ce644cacba7b4e6d7c3584eb
JavaFXpert <james@j1w.xyz>
2018-04-15 17:48:40 -0400

    Modify UI code

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -1,39 +1,40 @@
 let playingCards = [];
 let cardStr = ""; // Comma-delimited string of card codes held in the client
 
-// score nodes
+// mmessage display node
 let messageDisplayNode = document.getElementById("message-display");
-// let playerScoreNode = document.getElementById("player-number");
 
 // card area nodes
 let playingCardsNode = document.getElementById("playing-cards");
 
 // other nodes
-let announcementNode = document.getElementById("announcement");
-let newDeckNode = document.getElementById("new-game")
-let nextHandNode = document.getElementById("next-hand");
-let hitMeNode = document.getElementById("hit-me");
-let stayNode = document.getElementById("stay");
+let newDeckNode = document.getElementById("new-deck")
+let onlyHeartsNode = document.getElementById("only-hearts");
+let cutDeckNode = document.getElementById("cut-deck");
+let overhandShuffleNode = document.getElementById("overhand-shuffle");
+let riffleShuffleNode = document.getElementById("riffle-shuffle");
 let randomShuffleNode = document.getElementById("random-shuffle");
 let dealPokerHandNode = document.getElementById("deal-poker-hand");
 let shuffleDealNode = document.getElementById("shuffle-deal");
-let onlyHeartsNode = document.getElementById("only-hearts");
 
 
 // On click events
 newDeckNode.onclick = () => dealCards('newDeck');
-nextHandNode.onclick = () => dealCards('cutCards');
-hitMeNode.onclick = () => dealCards('overhandShuffle');
-stayNode.onclick = () => dealCards('riffleShuffle');
+onlyHeartsNode.onclick = () => dealCards('onlyHearts');
+cutDeckNode.onclick = () => dealCards('cutCards');
+overhandShuffleNode.onclick = () => dealCards('overhandShuffle');
+riffleShuffleNode.onclick = () => dealCards('riffleShuffle');
 randomShuffleNode.onclick = () => dealCards('randomShuffle');
 dealPokerHandNode.onclick = () => dealCards('dealPokerHand');
 shuffleDealNode.onclick = () => dealCards('shuffleDeal');
-onlyHeartsNode.onclick = () => dealCards('onlyHearts');
 
 function dealCards(modeArg) {
     var fetchStr = "http://localhost:8080/cards/deck/new?numcards=52";
     if (modeArg === "newDeck") {
         fetchStr = "http://localhost:8080/cards/deck/new?numcards=52";
+    }
+    else if (modeArg === "onlyHearts") {
+        fetchStr = "http://localhost:8080/cards/deck/hearts";
     }
     else if (modeArg === "cutCards") {
         fetchStr = "http://localhost:8080/cards/deck/cut?cards=" + cardStr;
@@ -53,13 +54,12 @@ function dealCards(modeArg) {
     else if (modeArg === "shuffleDeal") {
         fetchStr = "http://localhost:8080/cards/deck/shuffledeal?cards=" + cardStr;
     }
-    else if (modeArg === "onlyHearts") {
-        fetchStr = "http://localhost:8080/cards/deck/hearts";
-    }
     resetPlayingArea();
-    nextHandNode.style.display = "block";
-    hitMeNode.style.display = "block";
-    stayNode.style.display = "block";
+    newDeckNode.style.display = "block";
+    onlyHeartsNode.style.display = "block";
+    cutDeckNode.style.display = "block";
+    overhandShuffleNode.style.display = "block";
+    riffleShuffleNode.style.display = "block";
     randomShuffleNode.style.display = "block";
     dealPokerHandNode.style.display = "block";
     shuffleDealNode.style.display = "block";
@@ -87,33 +87,6 @@ function dealCards(modeArg) {
 
         })
         .catch(console.error)
-}
-
-
-function newHand() {
-  resetPlayingArea();
-
-  //fetch(`https://deckofcardsapi.com/api/deck/${deckID}/draw/?count=4`)
-  fetch(`http://localhost:8080/carddeck?shuffled=true&request=4`)
-  .then(res => res.json())
-  .then(res => {
-    // hitMeNode.style.display = "block";
-    // stayNode.style.display = "block";
-
-    messageDisplayNode.textContent = dealerScore;
-
-    playingCards.forEach((card, i) => {
-      let cardDomElement = document.createElement("img");
-      if(i===0) {
-        cardDomElement.src = 'http://127.0.0.1:8080/images/gray_back_reactor.png';
-      } else {
-        cardDomElement.src = card.image;
-      }
-      playingCardsNode.appendChild(cardDomElement)
-    })
-
-  })
-  .catch(console.error)
 }
 
 
